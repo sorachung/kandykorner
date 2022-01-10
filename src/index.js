d2f200a15ce1d717946f74a16647ab0e80b7ad99
sorachung <sorachung.src@gmail.com>
2022-01-10 16:07:15 -0600

    file directory structure; replaced App.js with KandyKorner.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { KandyKorner } from './components/KandyKorner';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <KandyKorner/>
   </React.StrictMode>,
   document.getElementById('root')
 );
